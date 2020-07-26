084643b9141d14085f054e7602745e5fd1910877
McMac <mcmac@McMacs-MacBook-Pro.local>
2020-07-26 21:22:43 +0200

    refactor and bug fixes

--- specutils/utils.py
+++ specutils/utils.py
@@ -4,19 +4,17 @@ from obspy import Stream
 from obspy import Trace
 
 
-def trim_resample_stream(st,t0,tN,dt):
+def trim_resample_stream(st,t0,tN,resamp):
 
-    _st = st.copy()
     for i in range(len(st)):
 
-        _utc_t0 = _st[i].stats.starttime
-        _t0 = _st[i].times()[0]
-        _tN = _st[i].times()[-1]
-        _samp = _st[i].stats.sampling_rate
-        _dt = 1/_samp
+        _resamp = st[i].stats.sampling_rate
+        _utc_t0 = st[i].stats.starttime
+        _t0 = st[i].times()[0]
+        _tN = st[i].times()[-1]
 
-        station = _st[i].stats.station
-        channel = _st[i].stats.channel
+        station = st[i].stats.station
+        channel = st[i].stats.channel
         tr_str = station + '.' + channel
         if t0 < _t0 or _tN < tN:
             print('There was an error trimming trace:',tr_str)
@@ -28,17 +26,15 @@ def trim_resample_stream(st,t0,tN,dt):
             print('f() _dt:',_dt)
             assert False
 
-        if dt != _dt:
-            _st[i].resample(int(1/dt))
+        if resamp != _resamp:
+            st[i].resample(resamp)
 
-        _st[i].trim(_utc_t0+t0,_utc_t0+t0+tN)
+        st[i].trim(_utc_t0+t0,_utc_t0+t0+tN)
 
-    return _st
+    return(_utc_t0+t0,_utc_t0+t0+tN)
 
 def bandpass_stream(st,f1,f2,nc=4):
-    _st = st.copy()
-    _st.filter('bandpass',freqmin=f1,freqmax=f2,corners=nc,zerophase=True)
-    return _st
+    st.filter('bandpass',freqmin=f1,freqmax=f2,corners=nc,zerophase=True)
 
 def conform_and_bandpass_streams_for_adjsrc(obs_st,syn_st,resamp,tN,f1,f2):
 
@@ -70,4 +66,40 @@ def conform_and_bandpass_streams_for_adjsrc(obs_st,syn_st,resamp,tN,f1,f2):
         assert o_tzero == 0.0 and s_tzero == 0.0
         assert o_tend == s_tend
 
+
+def conform_and_bandpass_syn_2_obs(obs_st,syn_st,f1,f2):
+
+    assert len(obs_st) == len(syn_st)
+    obs_st.sort()
+    syn_st.sort()
+
+    resamp = obs_st[0].stats.sampling_rate
+    t0 = syn_st[0].stats.starttime #use utc for syn j.i.c.
+    tN = t0 + obs_st[0].times()[-1]
+
+    syn_st.resample(resamp)
+    syn_st.filter('bandpass',freqmin=f1,freqmax=f2,corners=4,zerophase=True)
+
+    syn_st.trim(t0,tN)
+
+    #QC check
+    for i in range(len(syn_st)):
+
+        assert obs_st[i].stats.network == syn_st[i].stats.network
+        assert obs_st[i].stats.station == syn_st[i].stats.station
+        assert obs_st[i].stats.channel == syn_st[i].stats.channel
+
+        o_tzero = obs_st[i].times()[0] 
+        o_tend  = obs_st[i].times()[-1] 
+        s_tzero = syn_st[i].times()[0] 
+        s_tend  = syn_st[i].times()[-1] 
+        assert o_tzero == 0.0 and s_tzero == 0.0
+        if o_tend != s_tend:
+            station = syn_st[i].stats.station
+            channel = syn_st[i].stats.channel
+            tr_str = station + '.' + channel
+            print('There was an error conforming trace:',tr_str)
+            print('obs.tN =',o_tend)
+            print('syn.tN =',s_tend)
+            assert False
 
